d616064c2f1ef36657ed97b01a92c8fb90752710
Amila Madushanka Mapa Bandara <amilabandara8m@gmail.com>
2025-12-21 21:40:29 +1300

    Update talent competition files

--- App/Talent.App.WebApp/Program.cs
+++ App/Talent.App.WebApp/Program.cs
@@ -16,7 +16,7 @@ namespace Talent.App.WebApp
         {
             BuildWebHost(args).Run();
         }
-
+        //amila
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
